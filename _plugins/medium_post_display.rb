ab94ba817b7e7ca8f84d63c61a6820c9ffa1244e
Charles Kaminer <ckamine@g.clemson.edu>
2019-12-06 16:12:28 -0500

    Update some base information Refactor medium post retrieval plugin to leverage new gem versions

--- _plugins/medium_post_display.rb
+++ _plugins/medium_post_display.rb
@@ -1,4 +1,4 @@
-require 'feedjira'
+require 'httparty'
 
 module Jekyll
   class MediumPostDisplay < Generator
@@ -7,29 +7,20 @@ module Jekyll
     
     def generate(site)
       jekyll_coll = Jekyll::Collection.new(site, 'medium_posts')
-      site.collections['medium_posts'] = jekyll_coll
-      Feedjira::Feed.fetch_and_parse("https://medium.com/feed/@cjkaminer").entries.each do |e|
-        p "Title: #{e.title}, published on Medium #{e.url} #{e}"
-        title = e[:title]
-        path = "./_medium_posts/" + title + ".md"
+      entries = HTTParty.get('https://medium.com/feed/@cjkaminer')
+      entries["rss"]["channel"]["item"].each do |e|
+        title = e["title"]
+        p "Title: #{title}, fetched from Medium"
+        path = "./_medium_posts/#{title}.md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
         doc.data['title'] = title;
-        doc.data['content'] = e[:content];
-        doc.data['external_link'] = e[:url]
-        doc.data['date'] = e[:published]
-        doc.data['tags'] = e[:categories]
+        doc.data['external_link'] = e["link"]
+        doc.data['date'] = e["pubDate"]
+        doc.data['tags'] = e["category"]
         jekyll_coll.docs << doc
       end
+      site.collections['medium_posts'] = jekyll_coll
     end
   end
 end
-
-# entry properties
-# author -> string
-# categories -> array of strings (tags from medium)
-# content -> entire post (looks like html)
-# entry_id -> medium guid url
-# published -> utc date of publish
-# title -> string
-# url -> human readable url
--- medium_converter.js
+++
@@ -1,8 +0,0 @@
-const mediumToMarkdown = require('medium-to-markdown');
- 
-// Enter url here
-let postUrl = 'https://medium.com/@cjkaminer/creating-and-maintaining-password-security-with-bcrypt-8adff597288e'
-mediumToMarkdown.convertFromUrl(postUrl)
-.then(function (markdown) {
-  console.log(markdown); //=> Markdown content of medium post
-});

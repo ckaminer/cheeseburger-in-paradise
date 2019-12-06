52c0a315dbb356f42801981269f080d18b0b8e45
Charles Kaminer <ckamine@g.clemson.edu>
2019-12-06 17:10:01 -0500

    Create deployment script to publish static site to Github pages Github pages unable to use custom plugin (medium post retriever) - build site locally and push statically built site to separate Github repo for deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build site locally with custom plugins
+jekyll build
+
+# clear out current static site for clean slate
+rm -rf ../personal-site-static/*
+
+# move newly built _site into static site repository
+cp -R _site/* ../personal-site-static
+
+# publish updated static site to Github
+cd ../personal-site-static
+git add .
+git commit -m "Update static site"
+git push https://ckaminer:$GITHUB_PASS@github.com/ckaminer/ckaminer.github.io.git --all
